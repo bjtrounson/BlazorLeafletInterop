86a2a296982e4e7f3ede34b71310dc71c72ef01c
benjamint <benjamint@contecgroup.com>
2023-11-28 15:36:33 +1300

    Div Icon Support

---
+++ BlazorLeafletInterop/Models/Options/Basic/DivIconOptions.cs
@@ -0,0 +1,13 @@
+﻿using BlazorLeafletInterop.Models.Basics;
+
+namespace BlazorLeafletInterop.Models.Options.Basic;
+
+public class DivIconOptions
+{
+    public string? Html { get; set; }
+    public Point? BgPos { get; set; }
+    public Point? IconSize { get; set; }
+    public Point? IconAnchor { get; set; }
+    public Point? PopupAnchor { get; set; }
+    public string? ClassName { get; set; }
+}
